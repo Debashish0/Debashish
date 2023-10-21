d149127fc641cdb73c4d13eb9b6452dde2d0897c
Debashish Mohanty <121008381+Debashish0@users.noreply.github.com>
2023-10-21 08:56:02 +0530

    Update ArraySort.js

--- Javascript Program/ArraySort.js
+++ Javascript Program/ArraySort.js
@@ -6,7 +6,6 @@ function sortDescending(arr) {
         swapped = false;
         for (let i = 0; i < n - 1; i++) {
             if (arr[i] < arr[i + 1]) {
-                // Swap elements if they are in the wrong order
                 const temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;

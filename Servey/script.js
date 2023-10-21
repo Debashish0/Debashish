17b747cc4bd04106ddb5c9ec57b5cca75c8da361
Debashish Mohanty <121008381+Debashish0@users.noreply.github.com>
2023-10-21 08:57:33 +0530

    Update script.js

--- Servey/script.js
+++ Servey/script.js
@@ -1,32 +1,24 @@
 function validateForm() {
-    // Add validation logic here
     var email = document.getElementById("email").value;
     var mobile = document.getElementById("mobile").value;
-
-    // Validate email using a regular expression
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!emailPattern.test(email)) {
         alert("Please enter a valid email address.");
         return false;
     }
 
-    // Validate mobile number (assuming 10-digit format)
     var mobilePattern = /^\d{10}$/;
     if (!mobilePattern.test(mobile)) {
         alert("Please enter a valid 10-digit mobile number.");
         return false;
     }
-
-    // Show success message
     alert("Form submitted successfully!");
 
-    // Reset the form
     document.getElementById("surveyForm").reset();
 
-    return false; // Prevent actual form submission
+    return false;
 }
 
 function resetForm() {
-    // Reset the form
     document.getElementById("surveyForm").reset();
 }
